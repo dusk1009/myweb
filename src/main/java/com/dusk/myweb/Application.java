d2b216296a87f0544aedbc4aa55bc4eceebd66a3
dusk1009 <zgj1009@163.com>
2021-12-05 21:48:17 +0800

    create project

---
+++ src/main/java/com/dusk/myweb/Application.java
@@ -0,0 +1,8 @@
+package com.dusk.myweb;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
