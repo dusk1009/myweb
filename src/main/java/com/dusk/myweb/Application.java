95bece57a6ed070974a8be773f5efab07c5af754
Endless Dusk <zgj1009@163.com>
2021-12-09 21:01:16 +0800

    add service

--- src/main/java/com/dusk/myweb/Application.java
+++
@@ -1,8 +0,0 @@
-package com.dusk.myweb;
-
-@SpringBootApplication
-public class Application {
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
-}
